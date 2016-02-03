037edc7284a5ce588e33b8144ee3623dbdb33abf
Joe Vallillo <joevallillo@yahoo.com>
2016-02-02 19:58:14 -0500

    flickFindr v1

---
+++ FlickFindr/FlickFindrViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FlickFindrViewController.swift
+//  FlickFindr
+//
+//  Created by Joseph Vallillo on 1/28/16.
+//  Copyright © 2016 Joseph Vallillo. All rights reserved.
+//
+
+import Cocoa
+
+class FlickFindrViewController: UIViewController {
+
+}
